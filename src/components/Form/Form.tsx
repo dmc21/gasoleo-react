25c21c151de6e4855674013fd0e720c99c7855db
dmc21 <davidmoracaceres20@gmail.com>
2022-08-18 21:19:02 +0200

    town ready

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -6,26 +6,41 @@ import { provincias } from "../../data/provincias";
 export default function Form() {
 
 
-    const {setCodProv} = useContext(GasoleoContext)
+    const {findDataByProvince, findDataByTown, filteredTowns, codTown} = useContext(GasoleoContext)
 
     useEffect(() => {
         console.log("Initialize Form component")
     }, [])
 
-    const handleChange = (evt: { target: { value: string; }; }): void => {
-        setCodProv(evt.target.value);
+    const handleChangeProvince = (evt: { target: { value: string; }; }): void => {
+        findDataByProvince(evt.target.value);
+    }
+
+    const handleChangeTown = (evt: { target: { value: string; }; }): void => {
+        findDataByTown(evt.target.value);
     }
 
 
     return (
         <>
             <section>
-                <form>
-                    <select className="form-control w-auto" onChange={handleChange}>
+                <form className="d-flex flex-column gap-3 justify-content-center align-items-center">
+                    <select className="form-control w-auto" onChange={handleChangeProvince}>
                       {provincias.map(prov => {
                           return (
                           <>
                             <option key={prov.id} value={prov.id}>{prov.nm}</option>
+                          </>
+                          )
+                      })}
+                    </select>
+
+                    <select className="form-control w-auto" onChange={handleChangeTown}>
+                        <option value={'--'}>-- Selecciona localidad</option>
+                      {filteredTowns.map((town: any) => {
+                          return (
+                          <>
+                            <option selected={codTown === town.IDMunicipio} key={town.IDMunicipio} value={town.IDMunicipio}>{town.Municipio}</option>
                           </>
                           )
                       })}
--- src/context/GasoleoContext.tsx
+++ src/context/GasoleoContext.tsx
@@ -1,13 +1,17 @@
 import { createContext, useEffect, useState } from "react";
+import { localidades } from "../data/localidades";
 
 export const GasoleoContext = createContext({
   isGeoLocationActive: false,
-  setGeolocationActive : (bol: boolean): void => {},
+  setGeolocationActive: (bol: boolean): void => {},
   dataToShare: [],
-  codProv: '04',
+  codProv: "04",
+  codTown: "04",
   selectedOrderValue: "0",
-  setCodProv: (v: string): void => {},
-  sortAndFilterData: (order: string, dataProp?: any): void  => {}
+  findDataByProvince: (v: string): void => {},
+  findDataByTown: (v: string): void => {},
+  sortAndFilterData: (order: string, dataProp?: any): void => {},
+  filteredTowns: []
 });
 
 export function GasoleoContextProvider(props: any) {
@@ -15,9 +19,11 @@ export function GasoleoContextProvider(props: any) {
   const [data, setData] = useState([]);
   const [dataToShare, setDataToShare] = useState([]);
   const [codProv, setCodProv] = useState("04");
+  const [codTown, setCodTown] = useState("04");
+  const [filteredTowns, setFilteredTowns] = useState([])
   const [selectedOrderValue, setselectedOrderValue] = useState("0");
 
-  const arrayOrderStr: string [] = [
+  const arrayOrderStr: string[] = [
     "Precio Gasoleo A",
     "Precio Gasoleo Premium",
     "Precio Gasolina 95 E5",
@@ -25,8 +31,8 @@ export function GasoleoContextProvider(props: any) {
   ];
 
   useEffect(() => {
-    findDataByProvince();
-  }, [codProv]);
+    findDataByProvince("04");
+  }, []);
 
   const sortAndFilterData = (order: string, dataProp = null) => {
     let jsonData = dataProp || data;
@@ -36,21 +42,50 @@ export function GasoleoContextProvider(props: any) {
     setDataToShare(
       jsonData
         .filter((el) => el[arrayOrderStr[parseInt(order)]] !== "")
-        .sort((a: any [], b: any []) => {
-          const number1 =  parseFloat((a[arrayOrderStr[0] as any]).replace(",", "."))
-          const number2 =  parseFloat((b[arrayOrderStr[0] as any]).replace(",", "."))
-          return number1 - number2
+        .sort((a: any[], b: any[]) => {
+          const number1 = parseFloat(
+            a[arrayOrderStr[0] as any].replace(",", ".")
+          );
+          const number2 = parseFloat(
+            b[arrayOrderStr[0] as any].replace(",", ".")
+          );
+          return number1 - number2;
         })
     );
 
     setselectedOrderValue(order);
   };
 
-  const findDataByProvince = () => {
+  const findDataByTown = (town: string) => {
+
+    if (town === '--'){
+      findDataByProvince(codProv)
+      setCodTown(town)
+      return
+    }
+    setCodTown(town)
     fetch(
-      `https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/EstacionesTerrestres/FiltroProvincia/${codProv}`
+      `https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/EstacionesTerrestres/FiltroMunicipio/${town}`
     ).then((result) => {
       result.json().then((r) => {
+        sortAndFilterData(selectedOrderValue, r.ListaEESSPrecio);
+      });
+    });
+  }
+
+  const findDataByProvince = (prov: string) => {
+    setCodProv(prov)
+    fetch(
+      `https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/EstacionesTerrestres/FiltroProvincia/${prov}`
+    ).then((result) => {
+      result.json().then((r) => {
+        console.log(localidades, r)
+
+      const filteredAux = localidades.filter((data: any) => {
+          return r.ListaEESSPrecio.map((r: any) => r.IDMunicipio).includes(data.IDMunicipio)
+        }).filter((d: any) => d.IDProvincia.slice(0,2) === prov);
+
+        setFilteredTowns(filteredAux as [])
         sortAndFilterData(selectedOrderValue, r.ListaEESSPrecio);
       });
     });
@@ -64,9 +99,12 @@ export function GasoleoContextProvider(props: any) {
           setGeolocationActive,
           dataToShare,
           codProv,
-          setCodProv,
+          codTown,
+          findDataByProvince,
+          findDataByTown,
           selectedOrderValue,
           sortAndFilterData,
+          filteredTowns
         }}
       >
         {props.children}
